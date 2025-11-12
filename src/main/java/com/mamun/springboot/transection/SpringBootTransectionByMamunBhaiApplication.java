628579a95d756420367b046ae6323ad086859845
mostafizur <mostafiz.forhad4@gmail.com>
2025-11-12 21:12:06 +0600

    test

---
+++ src/main/java/com/mamun/springboot/transection/SpringBootTransectionByMamunBhaiApplication.java
@@ -0,0 +1,14 @@
+package com.mamun.springboot.transection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTransectionByMamunBhaiApplication {
+
+	public static void main(String[] args) {
+        System.out.println("HELLO");
+		SpringApplication.run(SpringBootTransectionByMamunBhaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mamun/springboot/transection/SpringBootTransectionByMamunBhaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mamun.springboot.transection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTransectionByMamunBhaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
