5e98af4c7258b9531c68e2b17deaad64d2c359bf
Mostafizur15 <mostafiz.forhad4@gmail.com>
2025-11-24 00:32:23 +0600

    basic trx

--- src/main/java/com/mamun/springboot/transection/SpringBootTransectionByMamunBhaiApplication.java
+++ src/main/java/com/mamun/springboot/transection/SpringBootTransectionByMamunBhaiApplication.java
@@ -1,14 +1,22 @@
 package com.mamun.springboot.transection;
 
+import com.mamun.springboot.transection.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootTransectionByMamunBhaiApplication {
-
+public class SpringBootTransectionByMamunBhaiApplication implements CommandLineRunner {
+	@Autowired
+	private AccountService accountService;
 	public static void main(String[] args) {
         System.out.println("HELLO");
 		SpringApplication.run(SpringBootTransectionByMamunBhaiApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		accountService.moneyTransfer(1,2,500.00);
+	}
 }
