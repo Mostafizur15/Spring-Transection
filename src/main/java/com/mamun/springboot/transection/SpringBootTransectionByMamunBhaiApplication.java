ac31080538144ee4ef5a0809e044d92e8cfbb008
mostafizur <mostafiz.forhad4@gmail.com>
2025-12-08 21:01:13 +0600

    Requred, requred_new. propagation

--- src/main/java/com/mamun/springboot/transection/SpringBootTransectionByMamunBhaiApplication.java
+++ src/main/java/com/mamun/springboot/transection/SpringBootTransectionByMamunBhaiApplication.java
@@ -1,6 +1,8 @@
 package com.mamun.springboot.transection;
 
 import com.mamun.springboot.transection.service.AccountService;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -8,6 +10,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootTransectionByMamunBhaiApplication implements CommandLineRunner {
+	@Autowired
+	private EntityManager entityManager;
+	@Autowired
+	private EntityManagerFactory emf;
 	@Autowired
 	private AccountService accountService;
 	public static void main(String[] args) {
@@ -18,5 +24,13 @@ public class SpringBootTransectionByMamunBhaiApplication implements CommandLineR
 	@Override
 	public void run(String... args) throws Exception {
 		accountService.moneyTransfer(1,2,500.00);
+		/*EntityManager em = emf.createEntityManager();
+		var tx = em.getTransaction();
+		tx.begin();
+		try{
+			tx.commit();
+		}catch (Exception e){
+			tx.rollback();
+		}*/
 	}
 }
--- src/main/java/com/mamun/springboot/transection/service/AccountService.java
+++ src/main/java/com/mamun/springboot/transection/service/AccountService.java
@@ -1,28 +1,43 @@
 package com.mamun.springboot.transection.service;
 
 import com.mamun.springboot.transection.entity.Account;
+import com.mamun.springboot.transection.entity.TxLog;
 import com.mamun.springboot.transection.repository.AccountRepository;
+import com.mamun.springboot.transection.repository.TxLogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import com.mamun.springboot.transection.service.TxLogService;
+
+import java.io.IOException;
 
 @Service
-@Transactional
+
 public class AccountService {
     @Autowired
     private AccountRepository accountRepository;
+    @Autowired
+    private TxLogRepository txLogRepository;
+    @Autowired
+    private TxLogService txLogService;
+    @Transactional(rollbackFor = IOException.class)
     public void moneyTransfer(Integer fromAccId, Integer toAccId, Double amount) {
 
         // credit operatio
-        Account creditAccount = accountRepository.findById(toAccId)
-                .orElseThrow(() -> new RuntimeException("account found by id = " + toAccId));
+        var txLog = new TxLog(null,fromAccId,toAccId,amount,null);
+
+        try{
+            Account creditAccount = accountRepository.findById(toAccId)
+                    .orElseThrow(() -> new RuntimeException("account found by id = " + toAccId));
+
 
         Double currentAmount = creditAccount.getBalance();
         creditAccount.setBalance(currentAmount + amount);
         accountRepository.save(creditAccount);
         accountRepository.flush();
         // debit operation
-        int v=1/0;
+        fun();
         Account debitAccount = accountRepository.findById(fromAccId)
                 .orElseThrow(() -> new RuntimeException("account found by id = " + fromAccId));
 
@@ -34,6 +49,20 @@ public class AccountService {
         debitAccount.setBalance(currentAmount2 - amount);
         accountRepository.save(debitAccount);
         accountRepository.flush();
+        txLog.setStatus("SUCCESS");
+    }catch(RuntimeException e){
+            txLog.setStatus("FAILED");
+          //  throw e;          // with out throw it will not roll back
+        }
+        finally {
+            txLogService.saveLog(txLog);
+        }
+    }
+
+    @Transactional(propagation=Propagation.REQUIRES_NEW)
+    void fun(){
+        int v=1/0;
     }
 
 }
+
